9262cd995bd6115046d2b7090ec3491e4ce10375
Charles Mwangi <bsclmr696619@spu.ac.ke>
2024-01-10 12:02:00 -0500

    modify

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
+
 def search_replace(my_list, search, replace):
-    if my_list is not None:
-        return([x if x != search else replace for x in my_list])
-    return None
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
